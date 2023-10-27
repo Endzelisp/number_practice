1f7ae31d1f970a1229ca30165ca5ee12a5f0e74c
Endzelisp <endzelisp@gmail.com>
2023-10-26 23:52:06 -0400

    Create business logic

--- main.js
+++ main.js
@@ -102,15 +102,62 @@ const numbersInLetters = [
   "cien"
 ];
 
+let isFinished = false;
+let randomNum;
+
 const displayedNumber = document.querySelector('.number-display #number');
 const numberInLetter = document.querySelector('.number-display figcaption');
+const inputUserAnswer = document.querySelector('.answer_submission input');
+const validationBtn = document.querySelector('.answer_submission button');
+const resultIcon = document.querySelector('.answer_submission #result-icon');
+
+validationBtn.textContent = 'Verificar';
 
 const getRandomNum = function() {
   return Math.floor(Math.random() * 101)
 }
 
-const randomNum = getRandomNum();
+randomNum = getRandomNum();
 
 window.addEventListener('DOMContentLoaded', function() {
   displayedNumber.textContent = randomNum;
+})
+
+const runValidResult = function() {
+  inputUserAnswer.classList.remove('wrong-answer');
+  inputUserAnswer.classList.add('valid-answer');
+  resultIcon.src = `./assets/party.gif`;
+}
+
+const runInvalidResult = function() {
+  inputUserAnswer.classList.add('wrong-answer');
+  resultIcon.src = `./assets/no.gif`
+}
+
+validationBtn.addEventListener('pointerdown', function() {
+  if (!isFinished) {
+    const userAnswer = inputUserAnswer.value.trim().toLowerCase();
+    const validAnswer = numbersInLetters[randomNum];
+    numberInLetter.textContent = validAnswer;
+    if (userAnswer === validAnswer) {
+      runValidResult()
+      isFinished = true;
+      this.textContent = 'otra vez'
+    } else {
+      runInvalidResult()
+    }
+    numberInLetter.textContent = validAnswer;
+    resultIcon.classList.add('icon-visible')
+    return
+  } else if (isFinished) {
+      randomNum = getRandomNum();
+      displayedNumber.textContent = randomNum;
+      validationBtn.textContent = 'Verificar';
+      numberInLetter.textContent = '';
+      inputUserAnswer.value = '';
+      inputUserAnswer.classList.remove('wrong-answer');
+      inputUserAnswer.classList.remove('valid-answer');
+      resultIcon.classList.remove('icon-visible')
+      isFinished = false;
+  }
 })
